546f96cebef12046198f4b972717e84e825a353a
BojanJankov <jankovbojan4@gmail.com>
2024-02-20 23:15:19 +0100

    update-workshop.js

--- Bojan workshop 2/app.js
+++ Bojan workshop 2/app.js
@@ -26,43 +26,56 @@ Add a filter by name, area, and population in descending order
 Add a filter by name, area, and population in ascending order
 */
 
-// URL links for feteching API data
+const app = () => {
+  // URL links for feteching API data
 
-const countryURL_ALL = "https://restcountries.com/v3.1/all";
+  const countryURL_ALL = "https://restcountries.com/v3.1/all";
+  const countryURL_NAME = `https://restcountries.com/v3.1/name/`;
 
-// Selectors
+  // Selectors
 
-// Search selectors
+  // Error selector
+  const errorContainer = document.querySelector(".error");
 
-const serachInput = document.querySelector(".searchInput");
-const searchButton = document.querySelector(".searchButton");
+  // Search selectors
+  const serachInput = document.querySelector(".searchInput");
+  const searchButton = document.querySelector(".searchButton");
 
-// Card selectors
-const cardContainer = document.querySelector(".all-card-container");
-const cardInfoDiv = document.querySelector(".countryInfo");
+  // Card selectors
+  const cardContainer = document.querySelector(".all-card-container");
 
-// Fetching data for all countries
+  // Fetching data for all countries
 
-const fetchCountryData = async (url) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+  // Global variable for country data
 
-    renderAllDataCountry(data, cardContainer);
+  let countryData = [];
 
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
+  const fetchCountryData = async (url) => {
+    spinner(true);
+    try {
+      const response = await fetch(url);
 
-// Function that render cards with data info for country
+      if (response.status === 404) throw "Country not found";
 
-const renderAllDataCountry = (data, element) => {
-  const cardHTML = data
-    .map((country) => {
-      return `
+      const data = await response.json();
+
+      renderAllDataCountry(data, cardContainer);
+
+      console.log(data);
+      spinner(false);
+      return data;
+    } catch (error) {
+      spinner(false);
+      throw new Error(error);
+    }
+  };
+
+  // Render function with HTML and data
+
+  const renderAllDataCountry = (data, element) => {
+    const cardHTML = data
+      .map((country) => {
+        return `
 <section class="card-container">
 <div class="flag">
     <img
@@ -75,71 +88,70 @@ const renderAllDataCountry = (data, element) => {
 <div class="countryInfo">
     <h4>Name:${country.name.common} </h4>
     <h4>Population: ${country.population}</h4>
-    <h4>Capital city: ${country.capital}</h4>
+    <h4>Capital city: ${country.capital ? country.capital[0] : "N/A"}</h4>    
     <h4>Area: ${country.area}</h4>
+    <h4>Currencies: ${
+      country.currencies
+        ? Object.keys(country.currencies)
+            .map((key) => country.currencies[key].name)
+            .join(", ")
+        : "N/A"
+    }</h4>      
+    <h4>Languages: ${
+      country.languages ? Object.values(country.languages).join(", ") : "N/A"
+    }</h4>
 </div>
 </section>
   `;
-    })
-    .join("");
+      })
+      .join("");
 
-  element.innerHTML = cardHTML;
+    element.innerHTML = cardHTML;
+
+    serachInput.value = "";
+  };
+
+  // Add event listener for search button that work on click, and show data in containers
+
+  searchButton.addEventListener("click", async () => {
+    try {
+      const searchValue = serachInput.value;
+      // If search input is empty, when someone click the button, all countries will be shown on screen in the containers
+      if (!searchValue) {
+        countryData = await fetchCountryData(countryURL_ALL);
+      }
+      // Render data by name function, display country that is searched by name
+      if (searchValue) {
+        countryData = await fetchCountryData(
+          `${countryURL_NAME}${searchValue}`
+        );
+      }
+
+      // Calling render function
+      renderAllDataCountry(countryData, cardContainer);
+    } catch (error) {
+      displayError(error, errorContainer);
+    }
+  });
+
+  // Error fucntion
+
+  const displayError = (error, element) => {
+    element.innerHTML = error;
+    serachInput.value = "";
+  };
+
+  // Spinner function
+
+  const spinner = (isShown) => {
+    const spinnerContainer = document.querySelector(".spinner-container");
+
+    if (isShown) {
+      spinnerContainer.classList.remove("hide");
+    } else {
+      spinnerContainer.classList.add("hide");
+    }
+  };
 };
 
-// Add event listener for search button that work on click, and show data in containers
-
-searchButton.addEventListener("click", () => {
-  // If search input is empty, when someone click the button, all countries will be shown on screen in the containers
-  if (serachInput.value === "") fetchCountryData(countryURL_ALL);
-  // Render data by name function, display country that is searched by name
-  fetchSerachByName(serachInput.value);
-});
-
-// Fetch function for countries that will be search by name, and will fetch the data by name of country and display in render function
-
-const countryURL_NAME = `https://restcountries.com/v3.1/name/`;
-
-const fetchSerachByName = async (searchValue) => {
-  try {
-    const response = await fetch(`${countryURL_NAME}${searchValue}`);
-    const data = await response.json();
-
-    renderCountryByName(data, cardContainer);
-
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Render html container with data of country that is searched by name
-
-const renderCountryByName = (data, element) => {
-  const cardHTML = data
-    .map((country) => {
-      return `
-<section class="card-container">
-<div class="flag">
-  <img
-      src="${country.flags.png}"
-      alt="${country.flags.alt}"
-      width="100px"
-      class="flag"
-  />
-</div>
-<div class="countryInfo">
-  <h4>Name:${country.name.common} </h4>
-  <h4>Population: ${country.population}</h4>
-  <h4>Capital city: ${country.capital}</h4>
-  <h4>Area: ${country.area}</h4>
-</div>
-</section>
-`;
-    })
-    .join("");
-
-  element.innerHTML = cardHTML;
-
-  // After showing data in container for country, refresh the value of search input
-  serachInput.value = "";
-};
+app();
